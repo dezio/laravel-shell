68f5a8fa69f07975317338a17314683f97632ca8
Dennis Ziolkowski <ziolkowski@prepaid-hoster.de>
2025-02-24 02:01:44 +0100

    Docs

--- src/Driver/Encoder/Base64Encoder.php
+++ src/Driver/Encoder/Base64Encoder.php
@@ -11,6 +11,12 @@ use DeZio\Shell\Contracts\CommandEncoder;
 
 class Base64Encoder implements CommandEncoder
 {
+    /**
+     * Encodes a given command into a base64 string and prepares it for execution in a bash-compatible format.
+     *
+     * @param string $command The command to be encoded.
+     * @return string The encoded command wrapped in a bash execution string.
+     */
     public function encode(string $command): string
     {
         $encoded = base64_encode($command);
